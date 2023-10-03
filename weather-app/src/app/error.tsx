45dd9ba9f061f397ae759943e2140006ea8b445e
허성진 <tjdwls75666@naver.com>
2023-10-04 00:34:10 +0900

    weather app 코드 업로드

---
+++ weather-app/src/app/error.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { type } from "os";
+import { useEffect } from "react";
+
+type Props = {
+  error: Error;
+  reset: () => void;
+};
+
+export default function Error({ error, reset }: Props) {
+  useEffect(() => {
+    console.error("---- " + error.message);
+  }, [error.message]);
+
+  return (
+    <div>
+      <h1>에러 페이지</h1>
+      <button
+        onClick={() => {
+          reset();
+        }}
+      >
+        새로고침
+      </button>
+    </div>
+  );
+}
---
+++ weather-app/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import HomeButton from "@/components/HomeButton";
+import "./global.css";
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "날씨 앱",
+  description: "날씨정보를 알려드립니다.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
